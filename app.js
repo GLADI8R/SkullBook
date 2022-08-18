83a82ada53ca26c283704aec0b5db9934d1b924a
GLADI8R <danishmehmuda@gmail.com>
2022-08-18 18:59:04 +0530

    add home route

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
 const app = express();
@@ -12,22 +11,24 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
-mongoose.connect("mongodb://localhost:27017/JSMedDB", {
-   useUnifiedTopology: true, 
-   useNewUrlParser: true
+// mongoose.connect("mongodb://localhost:27017/ReviewsDB", {
+//    useUnifiedTopology: true, 
+//    useNewUrlParser: true
+// });
+// mongoose.set('useNewUrlParser', true);
+// mongoose.set('useFindAndModify', false);
+// mongoose.set('useCreateIndex', true);
+// mongoose.set('useUnifiedTopology', true);
+
+// const blogSchema = new Schema({
+//    email: String,
+//    name: String,
+//    body: String
+// });
+
+app.get('/', (req, res) => {
+    res.render('Home');
 });
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
-
-const blogSchema = new Schema({
-   owner: String,
-   title: String,
-   body: String
-});
-
-
 
 app.listen(port, () => {
     console.log(`Server started at port: ${port}`);
